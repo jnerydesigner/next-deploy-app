2d8bbef98dc0bdac421e067d5bf1375d58c16665
Jander Nery <jander.webmaster@gmail.com>
2024-03-08 17:33:16 -0400

    not path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,6 @@ module.exports = {
       host : '191.101.78.119',
       ref  : 'origin/main',
       repo : 'git@github.com:jnerydesigner/next-deploy-app.git',
-      path : '/root/next-deploy-app',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js -env production',
       'pre-setup': '',
