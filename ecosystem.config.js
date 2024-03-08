9f7d3d193f207ecfd5f179bb1ed298948b225c04
Jander Nery <jander.webmaster@gmail.com>
2024-03-08 17:45:49 -0400

    add config e srtart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,6 @@
 module.exports = {
   apps : [{
+    name: 'deploy_app',
     script: 'npm start',
   }],
-
-  deploy : {
-    production : {
-      user : 'root',
-      host : '191.101.78.119',
-      ref  : 'origin/main',
-      repo : 'git@github.com:jnerydesigner/next-deploy-app.git',
-      path : '/root/source/next-deploy-app',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js -env production',
-      'pre-setup': '',
-      'ssh_options': 'ForwardAgent=yes',
-    }
-  }
 };
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone git@github.com:jnerydesigner/next-deploy-app.git
+
+npm run build
